54af2e2bae4580a242f848e974e536af0a77f291
akatsuki174 <dev2015js@gmail.com>
2017-04-02 11:37:19 +0900

    プロジェクトを作成

---
+++ UILiveCoding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UILiveCoding
+//
+//  Created by akatsuki174 on 2017/04/02.
+//  Copyright © 2017年 akatsuki174. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
